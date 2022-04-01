9183a6620d74575e4d61d78b45f957625210002a
Juan <jao9@alu.ua.es>
2022-04-01 05:27:09 +0200

    Añadida la clase CADUsuario DNI:20993208G

---
+++ library/CADUsuario.cs
@@ -0,0 +1,224 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using System.Data.Common;
+using System.Data.SqlClient;
+using System.Data.SqlTypes;
+using System.Configuration;
+
+namespace library
+{
+    class CADUsuario
+    {
+        private string constring;
+
+
+        public CADUsuario()
+        {
+            constring = ConfigurationManager.ConnectionStrings["Database"].ToString();
+
+        }
+
+        public bool createUsuario(ENUsuario en)
+        {
+            bool devolver = true;
+            try
+            {
+                SqlConnection conexion = new SqlConnection(constring);
+                conexion.Open();
+                SqlCommand com = new SqlCommand("Insert INTO [dbo].[Usuarios] (nombre, nif, edad) VALUES ('" + en._nombre + "', '" + en._nif + "', " + en._edad + ")");
+            }
+            catch (SqlException ex)
+            {
+                devolver = false;
+                Console.WriteLine("User operation has failed.Error: {0}", ex.Message);
+            }
+
+            return devolver;
+        }
+
+        public bool readUsuario(ENUsuario en)
+        {
+            bool devolver = true;
+            try
+            {
+                SqlConnection conexion = new SqlConnection(constring);
+                conexion.Open();
+                SqlCommand com = new SqlCommand("Select * from [dbo].[Usuarios] Where nif ='" + en._nif + "' ", conexion);
+                SqlDataReader buscar = com.ExecuteReader();
+                buscar.Read();
+
+                if (buscar["nif"].ToString() == en._nif)
+                {
+                    en._nif = buscar["nif"].ToString();
+                    en._nombre = buscar["nombre"].ToString();
+                    en._edad = int.Parse(buscar["edad"].ToString());
+                }
+                else
+                {
+                    devolver = false;
+                }
+
+                buscar.Close();
+                conexion.Close();
+            }
+            catch (SqlException ex)
+            {
+                devolver = false;
+                Console.WriteLine("User operation has failed.Error: { 0}", ex.Message);
+
+            }
+            return devolver;
+        }
+
+        public bool readFirstUsuario(ENUsuario en)
+        {
+            bool devolver = true;
+            try
+            {
+                SqlConnection conexion = new SqlConnection(constring);
+                conexion.Open();
+                SqlCommand com = new SqlCommand("Select * From [dbo].[Usuarios]", conexion);
+                SqlDataReader buscar = com.ExecuteReader();
+
+                buscar.Read();
+                en._nif = buscar["nif"].ToString();
+                en._nombre = buscar["nombre"].ToString();
+                en._edad = int.Parse(buscar["edad"].ToString());
+
+                devolver = true;
+                buscar.Close();
+                conexion.Close();
+
+            }
+            catch (SqlException ex)
+            {
+                devolver = false;
+                Console.WriteLine("User operation has failed.Error: { 0}", ex.Message);
+            }
+
+            return devolver;
+        }
+
+        public bool readNextUsuario(ENUsuario en)
+        {
+            bool devolver = true;
+            try
+            {
+                SqlConnection conexion = new SqlConnection(constring);
+                conexion.Open();
+                SqlCommand com = new SqlCommand("Select * From [dbo].[Usuarios]", conexion);
+                SqlDataReader buscar = com.ExecuteReader();
+
+                while (buscar.Read())
+                {
+                    if (en._nif.ToString() == buscar["nif"].ToString())
+                    {
+                        en._nif = buscar["nif"].ToString();
+                        en._nombre = buscar["nombre"].ToString();
+                        en._edad = int.Parse(buscar["edad"].ToString());
+                        break;
+                    }
+                }
+
+                devolver = true;
+                buscar.Close();
+                conexion.Close();
+            }
+            catch (SqlException ex)
+            {
+                devolver = false;
+                Console.WriteLine("User operation has failed.Error: { 0}", ex.Message);
+            }
+
+            return devolver;
+        }
+
+        public bool readPrevUsuario(ENUsuario en)
+        {
+            bool devolver = true;
+            try
+            {
+                SqlConnection conexion = new SqlConnection(constring);
+                conexion.Open();
+                SqlCommand com = new SqlCommand("Select * From [dbo].[Usuarios]", conexion);
+                SqlDataReader buscar = com.ExecuteReader();
+                buscar.Read();
+                ENUsuario resp = new ENUsuario();
+
+               
+                
+
+                while (buscar.Read()) {
+
+
+                    resp._nombre = buscar["nombre"].ToString();
+                    resp._nif = buscar["nif"].ToString();
+                    resp._edad = int.Parse(buscar["edad"].ToString());
+                    if(buscar["nif"].ToString() == en._nif)
+                    {
+                        break;
+                    }                
+                }
+                en._nif = resp._nif;
+                en._nombre = resp._nombre;
+                en._edad = resp._edad;
+
+                buscar.Close();
+                conexion.Close();
+                devolver = true;
+            }
+            catch (SqlException ex)
+            {
+                devolver = false;
+                Console.WriteLine("User operation has failed.Error: { 0}", ex.Message);
+            }
+
+            return devolver;
+        }
+
+        public bool updateUsuario(ENUsuario en)
+        {
+            bool devolver = true;
+            try
+            {
+                SqlConnection conexion = new SqlConnection(constring);
+                conexion.Open();
+                SqlCommand com = new SqlCommand("UPDATE [dbo].[Usuarios] SET nombre= '" + en._nombre + "' ,edad=" + en._edad + "WHERE nif = '" + en._nif + "'", conexion);
+                com.ExecuteNonQuery();
+                conexion.Close();
+            }
+            catch (SqlException ex)
+            {
+                devolver = false;
+                Console.WriteLine("User operation has failed.Error: { 0}", ex.Message);
+            }
+
+            return devolver;
+        }
+
+        public bool deleteUsuario(ENUsuario en)
+        {
+            bool devolver = true;
+            try
+            {
+                SqlConnection conexion = new SqlConnection(constring);
+                conexion.Open();
+                SqlCommand com = new SqlCommand("DELETE FROM [dbo].[Usuarios] WHERE nif = '" + en._nif + "'", conexion);
+                com.ExecuteNonQuery();
+                conexion.Close();
+                devolver = true;
+            }
+            catch (SqlException ex)
+            {
+                devolver = false;
+                Console.WriteLine("User operation has failed.Error: { 0}", ex.Message);
+            }
+
+            return devolver;
+        }
+    }
+}
---
+++ library/ENUsuario.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace library
+{
+    class ENUsuario
+    {
+        private string nif;
+        private string nombre;
+        private int edad;
+
+        public string _nif
+        {
+            get{ return nif; }
+            set{ nif = value; }
+
+        }
+
+        public string _nombre
+        {
+            get { return nombre; }
+            set { nombre = value; }
+        }
+
+        public int _edad
+        {
+            get { return edad; }
+            set { edad = value; }
+
+        }
+
+
+        public ENUsuario()
+        {
+            _nif = "";
+            _nombre = "";
+            _edad = 0;
+
+        }
+
+        public ENUsuario(string nif, string nombre, int edad)
+        {
+            _nif = nif;
+            _nombre = nombre;
+            _edad = edad;
+
+        }
+
+        public bool createUsuario()
+        {
+
+            return false;
+        }
+
+        public bool readUsuario()
+        {
+            return false;
+        }
+
+        public bool readFirstUsuario()
+        {
+
+            return false;
+        }
+
+        public bool readNextUsuario()
+        {
+            return false;
+        }
+
+        public bool readPrevUsuario()
+        {
+            return false;
+        }
+
+        public bool updateUsuario()
+        {
+            return false;
+        }
+
+        public bool deleteUsuario()
+        {
+            return false;
+        }
+    }
+}
